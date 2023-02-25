1f97f2dbe6bc324ac1263ca403f2d078b4d16eac
gyasis <gyasis@gmail.com>
2023-02-24 22:02:27 -0500

    finished functions section

--- files/rustlings/exercises/functions/functions1.rs
+++ files/rustlings/exercises/functions/functions1.rs
@@ -1,8 +1,13 @@
 // functions1.rs
 // Execute `rustlings hint functions1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
+
+fn call_me() {
+    println!("Ring! Call number {}", 1);
+}
 fn main() {
+    
     call_me();
 }
+
